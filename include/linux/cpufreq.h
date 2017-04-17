yerlirock/void-kernel

exynos: add UV interface (by AndreiLux)

include/linux/cpufreq.h
@@ -24,6 +24,9 @@
 
 #define CPUFREQ_NAME_LEN 16
 
+#if defined(CONFIG_CPU_EXYNOS4212) || defined(CONFIG_CPU_EXYNOS4412)
+#define CPUFREQ_LEVEL_END	(L17 + 1)
+#endif
 
 /*********************************************************************
  *                     CPUFREQ NOTIFIER INTERFACE                    *

xress7/mode

include/linux/cpufreq.h
@@ -24,6 +24,9 @@
 
 #define CPUFREQ_NAME_LEN 16
 
+#if defined(CONFIG_CPU_EXYNOS4212) || defined(CONFIG_CPU_EXYNOS4412)
+#define CPUFREQ_LEVEL_END	(L22 + 1)
+#endif
 
 /*********************************************************************
  *                     CPUFREQ NOTIFIER INTERFACE                    *